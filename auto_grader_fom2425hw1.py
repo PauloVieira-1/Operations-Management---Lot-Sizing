61bcd575ef9b311970ae8357db63b9df836cbe19
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-02-18 12:02:45 +0100

    all

---
+++ auto_grader_fom2425hw1.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  3 10:35:38 2024
+
+@author: rbroekme
+"""
+import sys
+import os
+from contextlib import contextmanager
+import pandas as pd
+from pylint import lint
+from importlib.metadata import version
+
+HOMEWORK = "fom2425hw1"
+NR_TESTS = 3                # Will increase during final grading
+
+@contextmanager
+def suppress_stdout():
+    """ Function to suppress output to console."""
+    with open(os.devnull, "w", encoding="utf-8") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+def unit_test(test_module, test_nr):
+    """ Unit tester."""
+    # Init return values
+    points = 0
+    obj_val = 0
+    setups = []
+    try:
+        if test_nr == 0:        # Given example
+            ex1_data = [20, 25, 14, 20, 15, 5.5, 2.5]
+            pt_data = [1.0, 2.0, 2.0, 1.5, 1.0, 0.5, 1.0]
+            obj_val, setups = test_module.lsp1(2, 50, pt_data, 25, ex1_data)
+            corr_setups = [0, 1, 0, 1, 1, 0, 0]
+            corr_obj_val = 330
+        if test_nr == 1:        # Given example
+            ex1_data = [20, 25, 14, 20, 15, 5.5, 2.5]
+            obj_val, setups = test_module.lsp2(2, 50, 5, ex1_data)
+            corr_setups = [1, 0, 1, 0, 1, 0, 0]
+            corr_obj_val = 261
+        if test_nr == 2:        # Given example
+            ex2_data = [[20, 5], [25, 10], [14, 21], [20, 20],
+                        [15, 7.5], [5.5, 11], [2.5, 13]]
+            obj_val, setups = test_module.lsp3(2, 50, [25, 25], ex2_data)
+            corr_setups = [0, 1, 2, 1, 0, 2, 0]
+            corr_obj_val = 461
+        if int(obj_val) == corr_obj_val:
+            points += 0.5
+        if setups == corr_setups:
+            points += 0.5
+        return points, obj_val, setups, None
+    except Exception as test_error:
+        return 0, obj_val, setups, str(test_error)
+
+def grade_submissions():
+    """ Code to check submitted file with unit tests. """
+
+    # Check version pylint
+    print(f"Current pylint version: {version('pylint')}")
+    print(f"Current GLPK version: {version('glpk_cmd')}")
+    # Clear the pylint cache
+    lint.pylinter.MANAGER.astroid_cache = {}
+    
+    # Collect the student submissions from the current directory
+    files = []
+    for file in os.listdir():
+        if file.endswith(".py"):
+            # Jump over the auto grader file
+            if file == 'auto_grader_' + HOMEWORK + '.py':
+                continue
+            files.append(os.path.splitext(file)[0])
+
+    files.sort(reverse=False)
+    print('Files to process:',  len(files))
+
+    # Dataframe with output of the functions per student group
+    df_res = pd.DataFrame(files, columns=['FileName'])
+    if len(files) > 10:
+        # Add columns for group IDs (only for batch grading)
+        df_res["Group"] = ""
+        df_res["ID1"] = ""
+        df_res["ID2"] = ""
+    # Add column for the grades
+    df_res["Grade"] = 0
+    df_res["Static"] = 0.
+    df_res["Dynamic"] = 0
+    # Add columns for each test
+    for test_nr in range(NR_TESTS):
+        df_res[f"U{test_nr}_pt"] = 0
+        df_res[f"U{test_nr}_return_0"] = ""
+        df_res[f"U{test_nr}_return_1"] = ""
+        df_res[f"U{test_nr}_error"] = ""
+
+    file_nr = 0
+    while file_nr < len(files):
+        if len(files) > 10:
+            # Only needed for the batch grading
+            group_data = files[file_nr].split("_")
+            df_res.at[file_nr, "Group"] = group_data[0]
+            df_res.at[file_nr, "ID1"] = group_data[1]
+            df_res.at[file_nr, "ID2"] = group_data[2]
+
+        # Get student file
+        group_module = __import__(files[file_nr])
+
+        # Init scores
+        static_score = 0.
+        dynamic_score = 0
+
+        with suppress_stdout():
+            try:
+                results = lint.Run([files[file_nr] + '.py'], do_exit=False)
+                static_score = results.linter.stats.global_note
+            except UnicodeError:
+                # Unknown diacriticals in student names
+                static_score = -1.         # Requires attention!
+        # Write static score
+        df_res.at[file_nr, "Static"] = static_score
+
+        # Execute unit tests
+        for test_nr in range(NR_TESTS):
+            points, return_0, return_1, error_str = unit_test(group_module, test_nr)
+            df_res.at[file_nr, f"U{test_nr}_pt"] = points
+            df_res.at[file_nr, f"U{test_nr}_return_0"] = return_0
+            df_res.at[file_nr, f"U{test_nr}_return_1"] = return_1
+            df_res.at[file_nr, f"U{test_nr}_error"] = error_str
+            dynamic_score += points
+        # Write dynamic score
+        df_res.at[file_nr, "Dynamic"] = dynamic_score
+
+        # Write grade
+        df_res.at[file_nr, 'Grade'] = round(0.2*static_score
+                                            + 8*(dynamic_score/NR_TESTS), 1)
+
+        file_nr += 1
+
+    # Write the results to Excel
+    df_res.to_excel(HOMEWORK + "_results.xlsx")
+
+if __name__ == '__main__':
+    grade_submissions()
---
+++ fom2425.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 08:36:34 2024
+
+@author: Rob Broekmeulen
+"""
+# Add to the import statement the functions you used
+from pulp import GLPK
+
+# Constants
+BIG_M = 100000
+
+def lsp1(cost_h, cost_k, cost_p, init_inv, requirements):
+    """Solving the uncapacitated lot sizing problem
+    with time-varying production costs (ULSP_TVP).
+
+    This function generates the ULSP_TVP formulation in PuLP
+    and solves it using the GLPK solver.
+
+    Parameters
+    ----------
+    cost_h       : holding costs h [EUR/kg/period]
+    cost_k       : fixed setup costs K [EUR/setup]
+    cost_p       : a list with the production costs P_t [EUR/kg]
+    init_inv     : the initial inventory [kg]
+    requirements : a list with the requirements [kg] for each period
+
+    Returns
+    -------
+    obj_val      : the objective value [EUR] after optimization
+    setups       : a list with the setup decision per period
+    """
+
+    # Define parameters
+    nr_periods = len(requirements)
+    # YOUR CODE HERE
+
+    # Declare model
+    model = None    # TEMPORARY: replace with your model declaration
+
+    # Add decision variables
+    # YOUR CODE HERE
+
+    # Add the objective function
+    # YOUR CODE HERE
+
+    # Add the constraints to the model
+    # YOUR CODE HERE
+
+    # Solve the model
+    # Default return values = No solution found
+    obj_val = 0
+    setups = [0]*nr_periods
+    if model is None:
+        # You did not decide to develop a optimization model
+        # Worst case: produce every period with a net requirement (=L4L)
+        obj_val = 0
+        inventory = init_inv
+        for per in range(nr_periods):
+            net_req = max(0, requirements[per] - inventory)
+            if net_req > 0:
+                setups[per] = 1
+                inventory += net_req
+                obj_val += cost_k + cost_p[per]*net_req
+            inventory -= requirements[per]
+            obj_val += cost_h*inventory
+        return obj_val, setups
+    # Solve the constructed model with GLPK within 10 seconds
+    model.solve(GLPK(msg=False, options=['--tmlim', '10']))
+    if model.status != 1:
+        # Model did not result in an optimal solution
+        return model.status, setups
+    # Retrieve the objective value
+    obj_val = model.objective.value()
+    # Retrieve the periods in which you decide to produce
+    # For example [1, 0, 0, 1, 0, 1] if you produce in periods 0, 3, and 5
+    # YOUR CODE HERE
+
+    return obj_val, setups
+
+def lsp2(cost_h, cost_k, init_inv, requirements):
+    """Solving the uncapacitated lot sizing problem
+    with non-consecutive setups (ULSP_NCS).
+
+    This function generates the ULSP_NCS formulation in PuLP
+    and solves it using the GLPK solver.
+
+    Parameters
+    ----------
+    cost_h       : holding costs h [EUR/kg/period]
+    cost_k       : fixed setup costs K [EUR/setup]
+    init_inv     : a list with the initial inventories [kg]
+    requirements : a list with the requirements [kg] for each period
+
+    Returns
+    -------
+    obj_val      : the objective value [EUR] after optimization
+    setups       : a list with the setup decision per period
+    """
+
+    # Define parameters
+    nr_periods = len(requirements)
+    # YOUR CODE HERE
+
+    # Declare model
+    model = None    # TEMPORARY: replace with your model declaration
+
+    # Add decision variables
+    # YOUR CODE HERE
+
+    # Add the objective function
+    # YOUR CODE HERE
+
+    # Add the constraints to the model
+    # YOUR CODE HERE
+
+    # Solve the model
+    # Default return values = No solution found
+    obj_val = 0
+    setups = [0]*nr_periods
+    if model is None:
+        # You did not decide to develop a model
+        # Worst case: produce every other period
+        obj_val = 0
+        inventory = init_inv
+        for per in range(nr_periods):
+            if per % 2 == 0:
+                # Even period
+                # Last period?
+                if per + 1 == nr_periods:
+                    req2 = requirements[per]
+                else:
+                    # Check for two periods
+                    req2 = requirements[per] + requirements[per + 1]
+                net_req = max(0, req2 - inventory)
+                if net_req > 0:
+                    setups[per] = 1
+                    obj_val += cost_k
+                    inventory += net_req
+            # Inventory
+            inventory -= requirements[per]
+            obj_val += cost_h*inventory
+        return obj_val, setups
+    # Solve the constructed model with GLPK within 10 seconds
+    model.solve(GLPK(msg=False, options=['--tmlim', '10']))
+    if model.status != 1:
+        # Model did not result in an optimal solution
+        return model.status, setups
+    # Retrieve the objective value
+    obj_val = model.objective.value()
+    # Retrieve the periods in which you decide to produce
+    # For example [1, 0, 1, 0, 1] if you produce in periods 0, 2, and 4
+    # YOUR CODE HERE
+
+    return obj_val, setups
+
+def lsp3(cost_h, cost_k, init_inv, requirements):
+    """Solving the uncapacitated multi-product lot sizing problem (UMLSP).
+
+    This function generates the UMLSP formulation in PuLP
+    and solves it using the GLPK solver.
+
+    Parameters
+    ----------
+    cost_h       : holding costs h [EUR/kg/period]
+    cost_k       : fixed setup costs K [EUR/setup]
+    init_inv     : a list with the initial inventories [kg]
+    requirements : a nested list with the requirements [kg] for each product
+                   in each each period
+
+    Returns
+    -------
+    obj_val      : the objective value [EUR] after optimization
+    setups       : a list with the setup decision per period
+    """
+
+    # Define parameters
+    nr_periods = len(requirements)
+    # YOUR CODE HERE
+
+    # Declare model
+    model = None    # TEMPORARY: replace with your model declaration
+
+    # Add decision variables
+    # YOUR CODE HERE
+
+    # Add the objective function
+    # YOUR CODE HERE
+
+    # Add the constraints to the model
+    # YOUR CODE HERE
+
+    # Solve the model
+    # Default return values = No solution found
+    obj_val = 0
+    setups = [0]*nr_periods
+    if model is None:
+        # You did not decide to develop a model
+        # Worst case: produce each product every other period
+        # if the net requirement is positive (=L4L)
+        # Note: no guarantee/check for feasibility!
+        obj_val = 0
+        inventory = []
+        inventory.append(init_inv[0])
+        inventory.append(init_inv[1])
+        prod = 0
+        for per in range(nr_periods):
+            if per + 1 == nr_periods:
+                req2 = requirements[per][prod]
+            else:
+                req2 = requirements[per][prod] + requirements[per+1][prod]
+            net_req = max(0, req2 - inventory[prod])
+            if net_req > 0:
+                setups[per] = prod + 1
+                inventory[prod] += net_req
+                obj_val += cost_k
+            # Inventory
+            inventory[0] -= requirements[per][0]
+            inventory[1] -= requirements[per][1]
+            obj_val += cost_h*(inventory[0] + inventory[1])
+            # Switch product
+            if prod == 0:
+                prod = 1
+            else:
+                prod = 0
+        return obj_val, setups
+    # Solve the constructed model with GLPK within 10 seconds
+    model.solve(GLPK(msg=False, options=['--tmlim', '10']))
+    if model.status != 1:
+        # Model did not result in an optimal solution
+        return model.status, setups
+    # Retrieve the objective value
+    obj_val = model.objective.value()
+    # Retrieve the periods in which you decide to produce
+    # For example [1, 2, 1, 0, 1] if you produce product 1 in periods 0, 2, and 4
+    # and product 2 in period 1
+    # YOUR CODE HERE
+
+    return obj_val, setups
+
+if __name__ == '__main__':
+    # Below you can experiment with your functions
+    ex1_data = [20, 25, 14, 20, 15, 5.5, 2.5]
+    pt_data = [1.0, 2.0, 2.0, 1.5, 1.0, 0.5, 1.0]
+    min_cost, setup_periods = lsp1(2, 50, pt_data, 25, ex1_data)
+    print(f"ULSP_TVP solution: costs={min_cost},"
+          +f" with setups {setup_periods}")
+
+    ex1_data = [20, 25, 14, 20, 15, 5.5, 2.5]
+    min_cost, setup_periods = lsp2(2, 50, 25, ex1_data)
+    print(f"ULSP_NCS solution: costs={min_cost},"
+          +f" with setups {setup_periods}")
+
+    ex2_data = [[20, 5], [25, 10], [14, 21], [20, 20],
+                [15, 7.5], [5.5, 11], [2.5, 13]]
+    min_cost, setup_periods = lsp3(2, 50, [25, 25], ex2_data)
+    print(f"UMLSP    solution: costs={min_cost},"
+          +f" with setups {setup_periods}")
